bd1f5c804dae0f87ef5ecb21c8d2d71d7e89ac28
dannycrews <dancrews@me.com>
2016-08-22 17:44:48 -0400

    Style battle results and refactor render containers

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,11 +5,12 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p>LOADING</p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player 1'>
@@ -21,30 +22,21 @@ function ConfirmBattle (props) {
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
-            <button
-              type='button'
-              className='btn btn-lg btn-success'
-              onClick={props.onInitiateBattle}>
-              Initiate Battle!
-            </button>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
           </div>
           <div className='col-sm-12' style={styles.space}>
             <Link to='/playerOne'>
-              <button
-                type='button'
-                className='btn btn-lg btn-danger'>
-                Reselect Players
-              </button>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
   isLoading: PropTypes.bool.isRequired,
   onInitiateBattle: PropTypes.func.isRequired,
-  playersInfo: PropTypes.array.isRequired
+  playersInfo: PropTypes.array.isRequired,
 }
 
 module.exports = ConfirmBattle;
